3ef52016334a079c807481f1fddbd5467cd991f2
vadimlulin <vlulin@borlas.ru>
2022-02-01 21:23:20 +0300

    Add kubernetes-2

--- kubernetes/terraform-k8s-cluster/waster/modules/node_groups/output.tf
+++
@@ -1,6 +0,0 @@
-output "node_group_ids" {
-  value = {
-    for group in yandex_kubernetes_node_group.cluster_node_groups:
-      group.name => group.id
-  }
-}
--- kubernetes/terraform-k8s-cluster/waster/output.tf
+++
@@ -1,9 +0,0 @@
-# output "node_group_ids" {
-#   value = module.node_groups.node_group_ids
-# }
-output "external_v4_endpoint" {
-  value = yandex_kubernetes_cluster.cluster.master[0].external_v4_endpoint
-}
-output "ca_certificate" {
-  value = yandex_kubernetes_cluster.cluster.master[0].cluster_ca_certificate
-}
